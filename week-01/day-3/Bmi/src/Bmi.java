d49df3c31c28bba34aec23363e1d0371d326ffd0
varistvan <istvanvaradi84@gmail.com>
2018-08-02 14:23:50 +0200

    task(Bmi): finish task

---
+++ week-01/day-3/Bmi/src/Bmi.java
@@ -0,0 +1,8 @@
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        System.out.println("Your body mass index is: " + massInKg/(heightInM*heightInM) + " kg/m^2");
+        // Print the Body mass index (BMI) based on these values
+    }
+}
