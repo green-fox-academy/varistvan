e0ce51f00ad34036854fbce68c9312bedd076d0e
varistvan <istvanvaradi84@gmail.com>
2018-08-02 14:03:17 +0200

    feat(CodingHours): finish task

--- week-01/day-3/CodingHours/src/CodingHours.java
+++ week-01/day-3/CodingHours/src/CodingHours.java
@@ -1,17 +1,8 @@
 public class CodingHours {
     public static void main(String[] args) {
-        // An average Green Fox attendee codes 6 hours daily
-        // The semester is 17 weeks long
-        //
-        // Print how many hours is spent with coding in a semester by an attendee,
-        // if the attendee only codes on workdays.
         int a = 6*5*17;
-        System.out.println("An average Green Fox attendee codes " + a + " hours in total.");
-        //
-        // Print the percentage of the coding hours in the semester if the average
-        // work hours weekly is 52
-        // 52-bol 30 orat kodol, vagyis
+        System.out.println("An average Green Fox attendee codes " + a + " hours in a semester.");
         double b = ((6*5)*100)/52.;
-        System.out.printf("An average Green Fox attendee spends with coding %.2f %% of the working hours", b);
+        System.out.printf("An average Green Fox attendee spends %.2f %% of the working hours with coding.", b);
     }
 }
