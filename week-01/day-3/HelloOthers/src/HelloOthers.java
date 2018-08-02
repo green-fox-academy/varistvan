b08e2d153e4d2ce25964a04918da0edd12eec22d
varistvan <istvanvaradi84@gmail.com>
2018-08-02 12:15:05 +0200

    Hello Others

---
+++ week-01/day-3/HelloOthers/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args){
+        System.out.println("Hello, Iringó!");
+        System.out.println("Hello, Krisztián!");
+        System.out.println("Hello, Tamás!");
+    }
+}
