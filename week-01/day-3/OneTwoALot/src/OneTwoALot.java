42a3e798d1f28bb8e5abb17a15c761599d1e48c8
varistvan <istvanvaradi84@gmail.com>
2018-08-02 16:36:29 +0200

    feat(OneTwoALot): finish task

---
+++ week-01/day-3/OneTwoALot/src/OneTwoALot.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+// Write a program that reads a number form the standard input,
+// If the number is zero or smaller it should print: Not enough
+// If the number is one it should print: One
+// If the number is two it should print: Two
+// If the number is more than two it should print: A lot
+
+public class OneTwoALot {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your number is: ");
+        int n = scanner.nextInt();
+        if(n<=0){
+            System.out.println("Not enough");
+        }else if(n==1){
+            System.out.println("One");
+        }else if(n==2){
+            System.out.println("Two");
+        }else{
+            System.out.println("A lot");
+        }
+    }
+}
