83b81027b6461877146d0ccc29a50f3de56390c6
varistvan <istvanvaradi84@gmail.com>
2018-08-04 18:54:04 +0200

    feat(DrawDiamond): modify task

--- week-01/day-4/DrawDiamond/src/DrawDiamond.java
+++ week-01/day-4/DrawDiamond/src/DrawDiamond.java
@@ -18,19 +18,20 @@ public class DrawDiamond{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type the number of lines in the diamond: ");
         int a = scanner.nextInt();
-        for(int i=1; i<=a; i++){
+        int i, j, k, l, m;
+        for(i=1; i<=a; i++){
             if(i <= (a+1)/2){
-                for(int j = (a+1)/2-i; j > 0; j--){
+                for(j = 0; j < (a+1)/2-i; j++){
                     System.out.print(" ");
                 }
-                for(int k = 1; k < (i*2); k++){
+                for(k = 1; k < (i*2); k++){
                     System.out.print("*");
                 }
             }else{
-                for(int l = 1; l < i-(a/2); l++){
+                for(l = 1; l < i-(a/2); l++){
                     System.out.print(" ");
                 }
-                for(int m = 1; m <= (a-i)*2+1; m++){
+                for(m = 1; m <= (a-i)*2+1; m++){
                     System.out.print("*");
                 }
             }
