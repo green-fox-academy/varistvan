54ce3e3b1a9acaf672c0fd86c0aa0e547b6015df
varistvan <istvanvaradi84@gmail.com>
2018-08-05 21:04:04 +0200

    feat(DrawDiagonal): finish task

--- week-01/day-4/DrawDiagonal/src/DrawDiagonal.java
+++ week-01/day-4/DrawDiagonal/src/DrawDiagonal.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class DrawSquare {
+public class DrawDiagonal {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type the number of lines in the square: ");
+        System.out.println("Enter the number of lines in the square: ");
         int a = sc.nextInt();
-        int i, j;
-        for(i=1; i<=a; i++){
-            for(j=1; j<=a; j++){
-                if((i==1 || i==a) || (j==1 || j==a)){
+        for (int i = 1; i <= a; i++){
+            for(int j = 1; j <= a; j++){
+                if((i==1 || i==a) || (j==1 || j==a) || (i==j)){
                     System.out.print("%");
                 }else{
                     System.out.print(" ");
