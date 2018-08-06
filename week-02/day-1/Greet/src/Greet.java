3f217d90f56b93eef904b21399a25b180d880074
varistvan <istvanvaradi84@gmail.com>
2018-08-06 14:55:53 +0200

    feat(Greet): finish task

---
+++ week-02/day-1/Greet/src/Greet.java
@@ -0,0 +1,9 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "GreenFox";
+        greet(al);
+    }
+    public static void greet(String theGreetedOne) {
+        System.out.println("Greetings, dear " + theGreetedOne);
+    }
+}
