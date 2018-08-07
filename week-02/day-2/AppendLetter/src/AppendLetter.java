20b4648982a859ea0345b260f0a9bdd885bda2ef
varistvan <istvanvaradi84@gmail.com>
2018-08-07 16:43:20 +0200

    feat(AppendLetter): finish task

--- week-02/day-2/AppendLetter/src/AppendLetter.java
+++ week-02/day-2/AppendLetter/src/AppendLetter.java
@@ -1,2 +1,16 @@
+import java.util.*;
+
 public class AppendLetter {
+    public static void main(String... args){
+        List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
+        System.out.println(appendA(far));
+    }
+
+    public static List<String> appendA(List<String> missingALetter) {
+        for(int i = 0; i < missingALetter.size(); i++) {
+            String tempWord = missingALetter.get(i) + 'a';
+            missingALetter.set(i, tempWord);
+        }
+        return missingALetter;
+    }
 }
