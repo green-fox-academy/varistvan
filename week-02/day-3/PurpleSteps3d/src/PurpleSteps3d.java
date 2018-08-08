8cbb70ce22c183e7aa419fc5c22e029e7dbc775b
varistvan <istvanvaradi84@gmail.com>
2018-08-08 18:42:15 +0200

    feat(PurpleSteps3d): finish task

---
+++ week-02/day-3/PurpleSteps3d/src/PurpleSteps3d.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3d {
+    public static void mainDraw(Graphics graphics) {
+        int counter = 0;
+        for (int i = 0; i < 6; i++) {
+            Color color = new Color(180, 60, 255);
+            graphics.setColor(color);
+            graphics.fill3DRect(i*10+counter, i*10+counter, (i+1)*10, (i+1)*10, true);
+            counter += (i*10);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
