1224e74d86737aa5ea348f11d3ae45c2a8abd5b5
varistvan <istvanvaradi84@gmail.com>
2018-08-15 17:15:17 +0200

    feat(DiceSet): finish task

---
+++ week-03/day-3/DiceSet/src/DiceSet.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class DiceSet {
+    // You have a `DiceSet` class which has a list for 6 dices
+    // You can roll all of them with roll()
+    // Check the current rolled numbers with getCurrent()
+    // You can reroll with reroll()
+    // Your task is to roll the dices until all of the dices are 6
+    int[] dices = new int[6];
+
+    public int[] roll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dices;
+    }
+
+    public int[] getCurrent() {
+        return dices;
+    }
+
+    public int getCurrent(int i) {
+        return dices[i];
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+    }
+
+    public void reroll(int k) {
+        dices[k] = (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        DiceSet diceSet = new DiceSet();
+        do {
+            diceSet.roll();
+            diceSet.getCurrent();
+        }
+        while (diceSet.getCurrent(0) != 6 | diceSet.getCurrent(1) != 6 | diceSet.getCurrent(2) != 6 | diceSet.getCurrent(3) != 6 | diceSet.getCurrent(4) != 6 | diceSet.getCurrent(5) != 6);
+        System.out.println(Arrays.toString(diceSet.getCurrent()));
+    }
+}
