b7a42ce75f7089d4a3c5efdc868daeb013d04434
varistvan <istvanvaradi84@gmail.com>
2018-08-15 13:28:25 +0200

    feat(PostIt): finish task

---
+++ week-03/day-3/PostIt/src/PostIt.java
@@ -0,0 +1,13 @@
+public class PostIt {
+
+    String backgroundColor;
+    String textColor;
+    String text;
+
+    PostIt(String backgroundColor, String textColor, String text) {
+        this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
+        this.text = text;
+    }
+
+}
