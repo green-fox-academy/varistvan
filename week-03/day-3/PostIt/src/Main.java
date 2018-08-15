55a891c274fe1816cba9a4b2d044684da89fcbfb
varistvan <istvanvaradi84@gmail.com>
2018-08-15 13:30:06 +0200

    feat(PostIt): modify task

--- week-03/day-3/PostIt/src/Main.java
+++ week-03/day-3/PostIt/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         PostIt postIt1 = new PostIt("orange", "blue", "Idea 1");
         PostIt postIt2 = new PostIt("pink", "black", "Awesome");
-        PostIt postIt3 = new PostIt("yellow", "green", "Superb");
+        PostIt postIt3 = new PostIt("yellow", "green", "Superb!");
         System.out.println(postIt1.text);
         System.out.println(postIt2.text);
         System.out.println(postIt3.text);
