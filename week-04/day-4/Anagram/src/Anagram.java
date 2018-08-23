d5ea06dee1124d0ff099e2199d4c035aec0f7964
varistvan <istvanvaradi84@gmail.com>
2018-08-23 17:47:31 +0200

    feat(Anagram): begin task

---
+++ week-04/day-4/Anagram/src/Anagram.java
@@ -0,0 +1,28 @@
+public class Anagram {
+    public static void main(String[] args) {
+        String word1 = "listen";
+        String word2 = "silent";
+        System.out.println(areAnagrams(word1, word2));
+    }
+    public static boolean areAnagrams(String s1, String s2){
+        String checker1;
+        String checker2;
+        int controlNr = 0;
+        if (s1.length() != s2.length()){
+            return false;
+        }else {
+            for (int i = 0; i < s1.length(); i++) {
+                for (int j = 0; j < s2.length() ; j++) {
+                    if (s1.charAt(i) == s2.charAt(j)) {
+                        controlNr++;
+                    }
+                }
+            }
+            if (controlNr != s1.length()) {
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+}
