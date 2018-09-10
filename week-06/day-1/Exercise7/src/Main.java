51414efc229334d187a83c8a192536087a98fb4c
varistvan <istvanvaradi84@gmail.com>
2018-09-10 17:04:56 +0200

    feat(Exercise7): finish task

--- week-06/day-1/Exercise7/src/Main.java
+++ week-06/day-1/Exercise7/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         String text = "Budapest Andrassy Avenue Sixtysix";
-        text.chars()
+
+        Map<Character, Long> countsCharacters = text.chars()
                 .mapToObj(c -> (char) c)
-                .filter(Character::isUpperCase)
-                .forEach(System.out::print);
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        countsCharacters.entrySet().stream()
+                .forEach(x -> System.out.println(x.getKey() + ": " + x.getValue()));
     }
 }
---
+++ week-06/day-1/Exercise8/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+    }
+}
