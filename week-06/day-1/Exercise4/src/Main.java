315fb1fb52b7055b0a95d0093767f6ab77f3f8d0
varistvan <istvanvaradi84@gmail.com>
2018-09-10 14:48:31 +0200

    feat(Exercise4): finish task

---
+++ week-06/day-1/Exercise4/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+        numbers.stream()
+                .filter(x -> x * x > 20)
+                .forEach(System.out::println);
+    }
+}
