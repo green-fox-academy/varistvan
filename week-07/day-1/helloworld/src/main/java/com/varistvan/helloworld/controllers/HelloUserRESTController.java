223feeb774405b990f798f3af80f00f3fcc859db
varistvan <istvanvaradi84@gmail.com>
2018-09-17 16:36:00 +0200

    feat(helloworld): HelloUserRESTController finished

---
+++ week-07/day-1/helloworld/src/main/java/com/varistvan/helloworld/controllers/HelloUserRESTController.java
@@ -0,0 +1,17 @@
+package com.varistvan.helloworld.controllers;
+
+import com.varistvan.helloworld.models.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloUserRESTController {
+
+    @RequestMapping("/greting")
+    public Greeting helloUser(@RequestParam(value = "name") String name) {
+            Greeting myGreeting = new Greeting(1, "Hello " + name);
+        return myGreeting;
+    }
+
+}
