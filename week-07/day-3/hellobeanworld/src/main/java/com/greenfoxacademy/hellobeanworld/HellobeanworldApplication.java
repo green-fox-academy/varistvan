b6a5408f4e139c1a5c45cd9db0bc996ef1a9e39b
varistvan <istvanvaradi84@gmail.com>
2018-09-19 14:42:07 +0200

    feat(hellobeanworld): finish task

---
+++ week-07/day-3/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.hellobeanworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication implements CommandLineRunner {
+    private Printer printer;
+
+    public HellobeanworldApplication(Printer printer) {
+        this.printer = printer;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobeanworldApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        printer.log("Hello!");
+    }
+}
