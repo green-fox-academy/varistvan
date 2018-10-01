daaf748e9dc8a7f5306177768a6788a48324789c
varistvan <istvanvaradi84@gmail.com>
2018-10-01 13:45:49 +0200

    feat(frontend) Doubling solved

--- week-09/day-1/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/DoublingController.java
+++ week-09/day-1/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/DoublingController.java
@@ -10,17 +10,26 @@ public class DoublingController {
 
     @GetMapping("/doubling")
     @ResponseBody
-    public Doubler doubling(@RequestParam int input) {
-        return new Doubler(input);
+    public Doubler doubling(@RequestParam(required = false) Integer input) {
+        if (input==null) {
+            return new Doubler();
+        } else {
+            return new Doubler(input);
+        }
     }
 
     static class Doubler {
         public int received;
         public int result;
+        public String error;
 
         public Doubler(int received) {
             this.received = received;
             this.result = 2 * received;
+        }
+
+        public Doubler() {
+            this.error= "Please provide an input!";
         }
     }
 
