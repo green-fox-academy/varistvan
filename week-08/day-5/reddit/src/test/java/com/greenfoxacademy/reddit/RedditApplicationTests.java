0b8b59539cdc3bea8a1257238233f9be0996de7c
varistvan <istvanvaradi84@gmail.com>
2018-10-02 16:16:32 +0200

    feat(reddit): add integration test

--- week-08/day-5/reddit/src/test/java/com/greenfoxacademy/reddit/RedditApplicationTests.java
+++ week-08/day-5/reddit/src/test/java/com/greenfoxacademy/reddit/RedditApplicationTests.java
@@ -2,15 +2,35 @@ package com.greenfoxacademy.reddit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class RedditApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    public void contextLoads() {
+    public void homePageLoads() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postApiTest() throws Exception {
+        mockMvc.perform(get("/submit"))
+                .andExpect(status().isOk());
     }
 
 }
---
+++ week-08/day-5/reddit/src/test/java/com/greenfoxacademy/reddit/controllers/MainControllerTest.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.reddit.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainControllerTest {
+
+    @Test
+    public void index() {
+    }
+
+    @Test
+    public void submitForm() {
+    }
+
+    @Test
+    public void submitSubmit() {
+    }
+
+    @Test
+    public void upvote() {
+    }
+
+    @Test
+    public void downvote() {
+    }
+}
